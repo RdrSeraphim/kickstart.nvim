5f4a9cc40b450cff07db13ff3df2de2fe40c8f68
Seraphim Pardee <me@srp.life>
2026-08-03 22:08:15 -0400

    update

---
+++ lua/custom/plugins/clojure.lua
@@ -0,0 +1,8 @@
+vim.pack.add {
+    'https://github.com/radenling/vim-dispatch-neovim',
+    'https://github.com/clojure-vim/vim-jack-in',
+    'https://github.com/Olical/conjure',
+}
+--require('vim-dispatch-neovim').setup = {}
+--require('vim-jack-in').setup = {}
+--require('conjure').setup = {}
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -2,19 +2,25 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 vim.pack.add {
-  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
-  'https://github.com/nvim-lua/plenary.nvim',
-  'https://github.com/MunifTanjim/nui.nvim',
+    { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
+    'https://github.com/nvim-lua/plenary.nvim',
+    'https://github.com/MunifTanjim/nui.nvim',
 }
 
 vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
 require('neo-tree').setup {
-  filesystem = {
-    window = {
-      mappings = {
-        ['\\'] = 'close_window',
-      },
+    filesystem = {
+        filtered_items = {
+            visible = true, -- This is required to show filtered items
+            hide_dotfiles = false,
+            hide_gitignored = false, -- Optional: set to false to also show git-ignored files
+        },
+        window = {
+            mappings = {
+                ['\\'] = 'close_window',
+            },
+        },
     },
-  },
 }
+
